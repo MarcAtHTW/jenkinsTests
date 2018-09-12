57a937f6a2f6f6da2f5f50578c6db9b1b9467988
MarcAtHTW <s0547375@htw-berlin.de>
2018-09-12 23:29:28 +0200

    Upload

---
+++ src/test/java/SampleTest.java
@@ -0,0 +1,24 @@
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class SampleTest {
+
+	@Test
+	public void simpleTestPass() {
+		int a = 1;
+		int b = 2;
+		int c;
+		
+		assertTrue(a + b == 3);
+	}
+	
+	@Test
+	public void simpleTestFail() {
+		int a = 1;
+		int b = 2;
+		int c;
+		
+		assertTrue(a + b == 4);
+	}
+}
